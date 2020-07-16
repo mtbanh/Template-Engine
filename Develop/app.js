190f4b8a4ca04c79bb2755b61b11e013adb40be1
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-15 21:41:28 -0700

    render employee profile to html

--- Develop/app.js
+++ Develop/app.js
@@ -4,8 +4,8 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const util = require('util')
-// const writeFileAsync = util.promisify(fs.writeFile());
+const util = require('util')
+const writeFileAsync = util.promisify(fs.writeFile);
 // const readFileAsync = util.promisify(fs.readFile());
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -13,9 +13,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employee= [];
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+const employees= [];
+
 function promptQs(){
     return inquirer.prompt([
         {
@@ -53,14 +52,15 @@ async function generateProfile(){
             switch(role){
                 case "Manager":
                     const officeNum = await inquirer.prompt({
-                        type: "input",
+                        type: "number",
                         message: "What's their office number?",
-                        name: "officeNum"
+                        name: "office"
                     })
-                    const { office } =  officeNum ;
+                    const { office } = officeNum ;
+                    console.log(officeNum)
                     const manager = new Manager (name, id, email, office);
-                    employee.push(manager);
-                    console.log(employee)
+                    employees.push(manager);
+                    console.log(employees)
                     break;
 
                 case "Engineer":
@@ -71,7 +71,7 @@ async function generateProfile(){
                     })
                     const { github } = githubName;
                     const engineer = new Engineer (name, id, email, github);
-                    employee.push(engineer);
+                    employees.push(engineer);
                     break;
 
                 case "Intern":
@@ -82,7 +82,7 @@ async function generateProfile(){
                     })
                     const { school } = schoolName;
                     const intern = new Intern (name, id, email, school);
-                    employee.push(intern);
+                    employees.push(intern);
                     break;
             }
 
@@ -100,8 +100,13 @@ async function generateProfile(){
             }
         }
         
-    //    const finalHTML = await render(employee);
-    //    await writeFileAsync(outputPath, finalHTML, 'utf-8');
+        function createTeam() {
+            if(!fs.existsSync(OUTPUT_DIR)){
+                fs.mkdirSync(OUTPUT_DIR)
+            }
+            fs.writeFileSync(outputPath, render(employees), "utf-8")
+        }
+        createTeam()
     }
     catch (err) {
         console.log(err);
@@ -111,22 +116,3 @@ async function generateProfile(){
 generateProfile();
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
