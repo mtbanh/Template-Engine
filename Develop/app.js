2639f74046203e46ae2accc341682104b763c6d2
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-15 17:46:04 -0700

    existed the loop when user don't have any more employee to add

--- Develop/app.js
+++ Develop/app.js
@@ -4,16 +4,16 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require('util')
-const writeFileAsync = util.promisify(fs.writeFile());
-const readFileAsync = util.promisify(fs.readFile());
+// const util = require('util')
+// const writeFileAsync = util.promisify(fs.writeFile());
+// const readFileAsync = util.promisify(fs.readFile());
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employee= []
+const employee= [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function promptQs(){
@@ -93,7 +93,9 @@ async function generateProfile(){
                 choices: ["yes", "no"]
             });
 
-            if(addEmployee === "no"){
+            console.log(addEmployee)
+
+            if(addEmployee.add === "no"){
                 wantsToCont = false;
             }
         }
