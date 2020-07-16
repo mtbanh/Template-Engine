2db66ecb93bd9a3f001154fcb573d96000b36c30
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-07-15 17:32:31 -0700

    created engineer and intern specific q's and all test passed

--- Develop/app.js
+++ Develop/app.js
@@ -4,13 +4,16 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require('util')
+const writeFileAsync = util.promisify(fs.writeFile());
+const readFileAsync = util.promisify(fs.readFile());
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employeeList= []
+const employee= []
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 function promptQs(){
@@ -45,6 +48,8 @@ async function generateProfile(){
         while (wantsToCont){
             const employeeProfile = await promptQs();
             const {name, id, email, role} = employeeProfile;
+            console.log(employeeProfile);
+
             switch(role){
                 case "Manager":
                     const officeNum = await inquirer.prompt({
@@ -52,20 +57,57 @@ async function generateProfile(){
                         message: "What's their office number?",
                         name: "officeNum"
                     })
-                    const { office } = officeNum;
+                    const { office } =  officeNum ;
                     const manager = new Manager (name, id, email, office);
-                    employeeList.push(manager);
+                    employee.push(manager);
+                    console.log(employee)
+                    break;
+
                 case "Engineer":
+                    const githubName = await inquirer.prompt({
+                        type: "input",
+                        message: "What's their github username?",
+                        name: "github"
+                    })
+                    const { github } = githubName;
+                    const engineer = new Engineer (name, id, email, github);
+                    employee.push(engineer);
+                    break;
+
                 case "Intern":
+                    const schoolName = await inquirer.prompt({
+                        type: "input",
+                        message: "Where did they go to school?",
+                        name: "school"
+                    })
+                    const { school } = schoolName;
+                    const intern = new Intern (name, id, email, school);
+                    employee.push(intern);
+                    break;
+            }
+
+            const addEmployee = await inquirer.prompt({
+                type: 'list',
+                message: "Add more empployee?",
+                name: "add",
+                choices: ["yes", "no"]
+            });
+
+            if(addEmployee === "no"){
+                wantsToCont = false;
             }
         }
+        
+    //    const finalHTML = await render(employee);
+    //    await writeFileAsync(outputPath, finalHTML, 'utf-8');
     }
     catch (err) {
         console.log(err);
     }
-}
+};
 
 generateProfile();
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
